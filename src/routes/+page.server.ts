933050896a812dec1752c859dfcd4c98f402c2c3
tzy <theanzy1@gmail.com>
2023-09-26 16:17:05 +0800

    dashboard load data

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,188 @@
+import { db } from '$lib/db';
+import { and, desc, eq, inArray, or, sql } from 'drizzle-orm';
+import { products, productsTransactions, transactions, users } from '../schema';
+import { getMonthName } from '$lib/utils';
+
+async function getTotalCustomers(year: number, month: number) {
+	const data = await db
+		.select({
+			count: sql<number>`sum(${users.id})::int`,
+			month: sql<number>`date_part('month', ${transactions.createdAt})`.mapWith((v) => {
+				return getMonthName(v);
+			}),
+			year: sql`date_part('year', ${transactions.createdAt})`
+		})
+		.from(users)
+		.innerJoin(transactions, eq(transactions.customerId, users.id))
+		.groupBy(
+			sql`date_part('year', ${transactions.createdAt})`,
+			sql`date_part('month', ${transactions.createdAt})`
+		)
+		.orderBy(desc(sql`date_part('month', ${transactions.createdAt})`))
+		.having(() => {
+			const isJanuary = month - 1 === 0;
+			const startMonth = isJanuary ? 12 : month - 1;
+			const startYear = isJanuary ? year - 1 : year;
+
+			return or(
+				and(
+					eq(sql`date_part('year', ${transactions.createdAt})`, startYear),
+					eq(sql`date_part('month', ${transactions.createdAt})`, startMonth)
+				),
+				and(
+					eq(sql`date_part('year', ${transactions.createdAt})`, year),
+					eq(sql`date_part('month', ${transactions.createdAt})`, month)
+				)
+			);
+		});
+	const end = data[0].count;
+	const start = data[1].count;
+	const changePercentage = Math.round(((end - start) / end) * 100);
+	return {
+		totalCustomers: end,
+		changePercentage
+	};
+}
+
+async function topSalesPerson(year: number, month: number) {
+	const salesQuery = db
+		.select({
+			affiliateId: users.id,
+			name: users.name,
+			salesMade:
+				sql<number>`sum(${products.price} * ${productsTransactions.quantity} / 100)::integer`.as(
+					'salesMade'
+				)
+		})
+		.from(users)
+		.innerJoin(transactions, eq(transactions.affiliateId, users.id))
+		.innerJoin(productsTransactions, eq(transactions.id, productsTransactions.transactionId))
+		.innerJoin(products, eq(products.id, productsTransactions.productId))
+		.groupBy(users.id)
+		.where(
+			and(
+				eq(sql`date_part('year', ${transactions.createdAt})`, year),
+				eq(sql`date_part('month', ${transactions.createdAt})`, month)
+			)
+		);
+
+	const sales = db.$with('sales').as(salesQuery);
+	const maxSalesQuery = db
+		.with(sales)
+		.select({
+			total: sql`max(${sales.salesMade})`.as('total')
+		})
+		.from(sales);
+	const topSales = await db
+		.with(sales)
+		.select({ name: sales.name, salesMade: sales.salesMade })
+		.from(sales)
+		.where(inArray(sales.salesMade, maxSalesQuery));
+	return topSales[0];
+}
+
+async function monthlySales(year: number, month: number) {
+	const salesQuery = db
+		.select({
+			year: sql`date_part('year', ${transactions.createdAt})`,
+			month: sql`date_part('month', ${transactions.createdAt})`,
+			salesMade:
+				sql<number>`sum(${products.price} * ${productsTransactions.quantity} / 100)::integer`.as(
+					'salesMade'
+				)
+		})
+		.from(users)
+		.innerJoin(transactions, eq(transactions.affiliateId, users.id))
+		.innerJoin(productsTransactions, eq(transactions.id, productsTransactions.transactionId))
+		.innerJoin(products, eq(products.id, productsTransactions.productId))
+		.groupBy(
+			sql`date_part('month', ${transactions.createdAt})`,
+			sql`date_part('year', ${transactions.createdAt})`
+		)
+		.having(() => {
+			const isJanuary = month - 1 === 0;
+			const startMonth = isJanuary ? 12 : month - 1;
+			const startYear = isJanuary ? year - 1 : year;
+			return or(
+				and(
+					eq(sql`date_part('year', ${transactions.createdAt})`, startYear),
+					eq(sql`date_part('month', ${transactions.createdAt})`, startMonth)
+				),
+				and(
+					eq(sql`date_part('year', ${transactions.createdAt})`, year),
+					eq(sql`date_part('month', ${transactions.createdAt})`, month)
+				)
+			);
+		})
+		.orderBy(desc(sql`date_part('month', ${transactions.createdAt})`));
+	const data = await salesQuery;
+	const end = data[0].salesMade;
+	const start = data[1].salesMade;
+	const changePercentage = Math.round(((end - start) / end) * 100);
+	return {
+		salesMade: end,
+		changePercentage
+	};
+}
+
+async function topSalesCountry(year: number, month: number, topo: Record<string, string>) {
+	const salesQuery = db
+		.select({
+			country: users.country,
+			salesMade:
+				sql<number>`sum(${products.price} * ${productsTransactions.quantity} / 100)::integer`.as(
+					'salesMade'
+				)
+		})
+		.from(users)
+		.innerJoin(transactions, eq(transactions.customerId, users.id))
+		.innerJoin(productsTransactions, eq(transactions.id, productsTransactions.transactionId))
+		.innerJoin(products, eq(products.id, productsTransactions.productId))
+		.groupBy(users.country)
+		.where(
+			and(
+				eq(sql`date_part('year', ${transactions.createdAt})`, year),
+				eq(sql`date_part('month', ${transactions.createdAt})`, month)
+			)
+		);
+
+	const sales = db.$with('sales').as(salesQuery);
+	const maxSalesQuery = db
+		.with(sales)
+		.select({
+			total: sql`max(${sales.salesMade})`.as('total')
+		})
+		.from(sales);
+	const topSales = await db
+		.with(sales)
+		.select({ country: sales.country, salesMade: sales.salesMade })
+		.from(sales)
+		.where(inArray(sales.salesMade, maxSalesQuery));
+	const result = {
+		country: topo[topSales[0].country],
+		revenue: topSales[0].salesMade
+	};
+	return result;
+}
+
+export async function load({ fetch }) {
+	const topo = await fetch(
+		'https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson'
+	)
+		.then((res) => res.json())
+		.then((topo) =>
+			topo.features.reduce((acc: Record<string, string>, f: any) => {
+				acc[f.id] = f.properties.name;
+				return acc;
+			}, {} as Record<string, string>)
+		);
+	const today = new Date();
+	const year = today.getUTCFullYear();
+	const month = today.getUTCMonth() + 1;
+	return {
+		customers: getTotalCustomers(year, month),
+		topSales: topSalesPerson(year, month),
+		monthlySales: monthlySales(year, month),
+		topSalesCountry: topSalesCountry(year, month, topo)
+	};
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,50 +1,151 @@
-<p>
-	Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos iure cum iusto aliquam
-	architecto corrupti, ipsa odit adipisci commodi est minima error hic magni? Voluptas modi aliquid
-	nam pariatur nemo. Vero quasi quae eligendi odio harum iusto quis sequi aliquam minima.
-	Repudiandae voluptate, explicabo quis minima libero recusandae obcaecati quo ex incidunt! Aut
-	facere et tenetur, accusantium labore cumque hic! Voluptates delectus optio libero asperiores
-	maxime? Assumenda consequuntur minus impedit soluta iste, hic laudantium non earum veniam nihil
-	provident aperiam pariatur aspernatur quas nemo voluptate asperiores nostrum rerum vel aut.
-	Blanditiis neque, ut animi fuga debitis minima consequuntur qui unde porro. Adipisci voluptas
-	inventore iste minima delectus molestias quas debitis enim saepe unde nisi ullam, possimus alias
-	deleniti et voluptatum? Recusandae iste vitae eum consectetur perspiciatis nobis deserunt mollitia
-	officiis enim voluptatem quo dignissimos at veniam eius ea, voluptates omnis pariatur odit
-	molestiae dolor tempora magnam reprehenderit sint ipsam! Nam? At, placeat eius voluptates tempora
-	illo error cum aliquid minus quibusdam aut laboriosam dolor? Architecto minima id eum nostrum,
-	placeat, expedita hic, vitae possimus velit quam nesciunt. Rem, explicabo excepturi. Vel in
-	distinctio reprehenderit unde, dolor reiciendis quia expedita ullam nam. Perferendis inventore
-	rerum obcaecati aut, mollitia ratione expedita tempora, quod rem repellendus fugiat maiores
-	doloremque voluptate illum, eius beatae. Beatae doloribus ut quisquam illo, provident iure nostrum
-	rerum atque non vitae quasi! Corrupti eveniet ratione laudantium non exercitationem dicta
-	consequatur sapiente culpa reiciendis rerum! Omnis ipsa consectetur mollitia architecto! Impedit
-	vero ea ipsum doloribus repellendus minus eligendi nemo amet maxime porro beatae quisquam aut
-	distinctio voluptatum assumenda dolores, fugit tempora eaque nulla consequatur consequuntur quod
-	saepe et ullam! Doloribus. Veniam recusandae rem, reprehenderit doloribus necessitatibus natus.
-	Consectetur a architecto autem deserunt eligendi quibusdam sequi? Autem unde error expedita
-	nesciunt. Repellendus quos ratione eius quaerat sapiente impedit mollitia magnam omnis. Dolorum
-	dolores tempora natus id iusto repellat modi eaque, odio libero veritatis necessitatibus aliquam
-	aperiam. Deserunt tenetur cupiditate, eum at architecto, necessitatibus doloremque earum ad quis
-	perspiciatis facilis illum quam. Autem sapiente corporis libero natus rem rerum labore odio
-	consequuntur, recusandae ab possimus itaque? Praesentium soluta optio nostrum commodi doloremque
-	consectetur culpa reprehenderit autem vero cupiditate, quasi a voluptates dicta? Inventore et
-	perferendis ab ea sunt labore pariatur dolorum nobis architecto rem? Odio natus rem ipsum quo,
-	numquam tenetur veniam aspernatur delectus suscipit obcaecati unde aliquid enim ut excepturi
-	commodi? Ipsa repellendus provident assumenda exercitationem commodi. Alias, architecto. Excepturi
-	sint eaque dolorem consequuntur corrupti laborum nulla officia ipsa, labore, repellendus commodi
-	a, vero nihil illo quia at! Ducimus, sequi tempore! Libero officiis fuga sint delectus facere
-	distinctio blanditiis, voluptatem unde maxime nemo earum itaque molestiae natus illum commodi eum
-	et? Quibusdam, doloribus. At molestias dicta voluptas sed. Ullam, voluptatem vel? Fuga magni
-	corporis, ut quis fugit in porro velit inventore ratione, tempora, assumenda nobis officiis quod
-	voluptas doloremque dolore? Dolore velit dicta soluta voluptatibus beatae ab. Eum tempora iusto
-	ipsa. Similique iusto corporis voluptatum architecto voluptate explicabo nemo nesciunt tempora
-	quis quo alias dolore unde inventore vero tempore libero aut atque, voluptas ad expedita
-	perspiciatis? Dolorem officiis distinctio repellendus dolor. Obcaecati dignissimos repudiandae
-	quia nostrum, fugit facere modi porro vitae voluptatem nobis aliquid veritatis voluptas! Quis,
-	tempora libero. Dolores veritatis facilis optio tenetur doloribus eveniet ipsum at ea culpa
-	assumenda. Dolor commodi corrupti consequatur laborum enim ea corporis cumque cupiditate, aut
-	aliquid officiis. Fuga ab aspernatur fugit ipsa praesentium optio. Repellendus architecto
-	molestias ut est ratione asperiores nostrum dolorem odit? Itaque sunt nisi, rerum quo est
-	repudiandae facere animi voluptate quibusdam dolore aliquam voluptatem ut porro ipsa autem dolor
-	nesciunt ullam, dicta nihil numquam totam recusandae corporis, laudantium sed. Sunt.
-</p>
+<script lang="ts">
+	import { currencyFormatter, unitFormatter } from '$lib/utils.js';
+
+	// your script goes here
+	export let data;
+</script>
+
+<h2 class="h3 font-bold">Dashboard</h2>
+<h5 class="h6 text-surface-600-300-token mb-5">Welcome to your dashboard</h5>
+
+<div class="grid grid-cols-6 grid-rows-6 gap-4">
+	<div
+		class="flex flex-col border border-surface-300-600-token bg-neutral-200 dark:bg-surface-800 shadow-sm p-4 gap-y-4 justify-between rounded"
+	>
+		<div class="flex flex-row justify-between items-center text-surface-600-300-token">
+			<p class="text-sm">Total Customers</p>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				width="48"
+				height="48"
+				viewBox="0 0 48 48"
+				class="w-6 h-6"
+				><path
+					fill="currentColor"
+					d="M30.27 25c-.24 0-.49-.04-.73-.12A2.211 2.211 0 0 1 28 22.75v-2.81c-2.26-.36-4-2.33-4-4.69v-6.5C24 6.13 26.13 4 28.75 4h11.5C42.87 4 45 6.13 45 8.75v6.5c0 2.62-2.13 4.75-4.75 4.75h-5.12l-3.08 4.1c-.44.58-1.09.9-1.78.9ZM28.75 6.5c-1.24 0-2.25 1.01-2.25 2.25v6.5c0 1.24 1.01 2.25 2.25 2.25h1.75V22l3.38-4.5h6.38c1.24 0 2.25-1.01 2.25-2.25v-6.5c0-1.24-1.01-2.25-2.25-2.25H28.75ZM16 27.5c-4.14 0-7.5-3.36-7.5-7.5c0-4.14 3.36-7.5 7.5-7.5c4.14 0 7.5 3.36 7.5 7.5c0 4.14-3.36 7.5-7.5 7.5ZM16 15c-2.76 0-5 2.24-5 5s2.24 5 5 5s5-2.24 5-5s-2.24-5-5-5ZM3 34.79c0 .09.15 9.21 13 9.21s13-9.12 13-9.21v-1.04c0-2.07-1.68-3.75-3.75-3.75H6.75C4.68 30 3 31.68 3 33.75v1.04Zm2.5-1.04c0-.69.56-1.25 1.25-1.25h18.5c.69 0 1.25.56 1.25 1.25v1.03c0 .27-.22 6.72-10.5 6.72S5.5 35.05 5.5 34.79v-1.04Z"
+				/></svg
+			>
+		</div>
+		<p class="font-bold text-2xl dark:text-tertiary-700 text-tertiary-600">
+			{unitFormatter.format(data.customers.totalCustomers)}
+		</p>
+		<div class="flex justify-between items-center text-sm text-surface-600-300-token">
+			<p
+				class={data.customers.changePercentage > 0
+					? 'dark:text-tertiary-900-300-token'
+					: 'text-red-700 text-tertiary-600'}
+			>
+				{data.customers.changePercentage > 0
+					? `+${data.customers.changePercentage}%`
+					: `${data.customers.changePercentage}%`}
+			</p>
+			<p>Since last month</p>
+		</div>
+	</div>
+	<div
+		class="flex flex-col border border-surface-300-600-token bg-neutral-200 dark:bg-surface-800 shadow-sm p-4 gap-y-4 justify-between rounded"
+	>
+		<div class="flex flex-row justify-between items-center text-surface-600-300-token">
+			<p class="text-sm">Top Sales</p>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				width="24"
+				height="24"
+				viewBox="0 0 24 24"
+				class="w-6 h-6"
+				><path
+					fill="currentColor"
+					d="M7 21v-2h4v-3.1q-1.225-.275-2.188-1.038T7.4 12.95q-1.875-.225-3.138-1.637T3 8V7q0-.825.588-1.413T5 5h2V3h10v2h2q.825 0 1.413.588T21 7v1q0 1.9-1.263 3.313T16.6 12.95q-.45 1.15-1.413 1.913T13 15.9V19h4v2H7Zm0-10.2V7H5v1q0 .95.55 1.713T7 10.8Zm10 0q.9-.325 1.45-1.088T19 8V7h-2v3.8Z"
+				/></svg
+			>
+		</div>
+		<div class=" dark:text-tertiary-700 text-tertiary-600">
+			<p class="font-bold text-2xl">
+				{data.topSales.name}
+			</p>
+		</div>
+		<div class="flex justify-between items-center text-sm text-surface-600-300-token">
+			<p class="text-tertiary-700-200-token">
+				{currencyFormatter.format(data.topSales.salesMade)}
+			</p>
+			<p>This month</p>
+		</div>
+	</div>
+	<div
+		class="flex flex-col border border-surface-300-600-token bg-neutral-200 dark:bg-surface-800 shadow-sm p-4 gap-y-4 justify-between rounded"
+	>
+		<div class="flex flex-row justify-between items-center text-surface-600-300-token">
+			<p class="text-sm">Monthly Sales</p>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				width="48"
+				height="48"
+				viewBox="0 0 48 48"
+				class="w-6 h-6"
+				><g fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="4"
+					><path d="M41 14L24 4L7 14v20l17 10l17-10V14Z" /><path
+						stroke-linecap="round"
+						d="M24 22v8m8-12v12m-16-4v4"
+					/></g
+				></svg
+			>
+		</div>
+		<div class="dark:text-tertiary-700 text-tertiary-600">
+			<p class="font-bold text-2xl">
+				{currencyFormatter.format(data.monthlySales.salesMade)}
+			</p>
+		</div>
+		<div class="flex justify-between items-center text-sm text-surface-600-300-token">
+			<p
+				class={data.monthlySales.changePercentage > 0
+					? 'dark:text-tertiary-900-300-t7ke text-tertiary-600n'
+					: 'text-red-600'}
+			>
+				{data.monthlySales.changePercentage > 0
+					? `+${data.monthlySales.changePercentage}%`
+					: `${data.monthlySales.changePercentage}%`}
+			</p>
+			<p>Since last month</p>
+		</div>
+	</div>
+	<div
+		class="flex flex-col border border-surface-300-600-token bg-neutral-200 dark:bg-surface-800 shadow-sm p-4 gap-y-4 justify-between rounded"
+	>
+		<div class="flex flex-row justify-between items-center text-surface-600-300-token">
+			<p class="text-sm">Top Market</p>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				width="48"
+				height="48"
+				viewBox="0 0 48 48"
+				class="w-6 h-6"
+				><g fill="currentColor"
+					><path
+						fill-rule="evenodd"
+						d="M9.263 6c-.378 0-.715.262-.845.656L6.11 13.667a2.2 2.2 0 0 0-.11.687v2.789C6 18.72 7.151 20 8.571 20c1.42 0 2.572-1.28 2.572-2.857c0 1.578 1.151 2.857 2.571 2.857c1.42 0 2.572-1.28 2.572-2.857c0 1.578 1.151 2.857 2.571 2.857c1.42 0 2.57-1.278 2.572-2.855C21.429 18.722 22.58 20 24 20c1.42 0 2.571-1.28 2.571-2.857c0 1.578 1.152 2.857 2.572 2.857c1.42 0 2.57-1.278 2.571-2.855c.001 1.577 1.152 2.855 2.572 2.855c1.42 0 2.571-1.28 2.571-2.857c0 1.578 1.151 2.857 2.572 2.857C40.849 20 42 18.72 42 17.143v-2.789a2.2 2.2 0 0 0-.11-.687l-2.308-7.01c-.13-.395-.467-.657-.845-.657H9.263Z"
+						clip-rule="evenodd"
+					/><path
+						fill-rule="evenodd"
+						d="M10 21.23V29H7.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h33a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5H38v-7.77a3.916 3.916 0 0 1-1.143-.703a3.953 3.953 0 0 1-.857.576V29H12v-7.897a3.953 3.953 0 0 1-.857-.576c-.336.295-.72.535-1.143.703Zm26-1.957c.085-.085.165-.176.24-.273H36v.273ZM37.474 19a2.75 2.75 0 0 0 .526.519V19h-.526ZM10 19.519a2.63 2.63 0 0 0 .526-.519H10v.519ZM11.76 19H12v.273a2.766 2.766 0 0 1-.24-.273ZM8.5 33a.5.5 0 0 0-.5.5V41a1 1 0 0 0 1 1h30a1 1 0 0 0 1-1v-7.5a.5.5 0 0 0-.5-.5h-31Z"
+						clip-rule="evenodd"
+					/><path
+						d="M14 26.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-2Z"
+					/><path
+						d="M16 27.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1Zm8 0a1.5 1.5 0 1 1-3 0a1.5 1.5 0 0 1 3 0Z"
+					/></g
+				></svg
+			>
+		</div>
+		<div class="dark:text-tertiary-700 text-tertiary-600">
+			<p class="font-bold text-2xl">
+				{data.topSalesCountry.country}
+			</p>
+		</div>
+		<div class="flex justify-between items-center text-sm text-surface-600-300-token">
+			<p class="text-tertiary-700-200-token">
+				{currencyFormatter.format(data.topSalesCountry.revenue)}
+			</p>
+			<p>This month</p>
+		</div>
+	</div>
+</div>
